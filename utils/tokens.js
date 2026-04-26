ff49bcbeb04c14e30dd0316e31eaca5277690edb
Samuel Dauda <smartsam9981@gmail.com>
2026-04-26 13:46:42 +0100

    updated token.js

--- utils/tokens.js
+++ utils/tokens.js
@@ -1,35 +1,35 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
-const ACCESS_EXPIRY = '3m';
-const REFRESH_EXPIRY='5m',
+const ACCESS_EXPIRY = '3m';   // 3 minutes as per spec
+const REFRESH_EXPIRY = '5m';  // 5 minutes as per spec
 
-function generateAccessToken(user){
-    return jwt.sign(
-        {id: user.id, role: user.role, username: user.username},
-        process.env.JWT_ACCESS_SECRET,
-        {expiresIn: ACCESS_EXPIRY}
-    );
+function generateAccessToken(user) {
+  return jwt.sign(
+    { id: user.id, role: user.role, username: user.username },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: ACCESS_EXPIRY }
+  );
 }
 
-function generateRefreshToken(user){
-    return jwt.sign(
-        {id: user.id},
-        process.env.JWT_REFRESH_SECRET,
-        {expiresIn: REFRESH_EXPIRY}
-    );
+function generateRefreshToken(user) {
+  return jwt.sign(
+    { id: user.id },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: REFRESH_EXPIRY }
+  );
 }
 
-function verifyAccessToken(token){
-    return jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+function verifyAccessToken(token) {
+  return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
 }
 
-function verifyRefreshToken(token){
-    return jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+function verifyRefreshToken(token) {
+  return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
 }
 
 module.exports = {
-    generateAccessToken,
-    generateRefreshToken,
-    verifyAccessToken,
-    verifyRefreshToken
-}
+  generateAccessToken,
+  generateRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken
+};
